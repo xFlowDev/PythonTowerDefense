60b7568d9f03bbdc273f41fe10fc8102bd1754e0
xFlowDev <xflowdev@protonmail.com>
2019-12-21 13:42:49 +0100

    Initialized pygame and displaying empty window

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
-import pygame
+import pygame, sys
+
+
+class Game:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.size = width, height
+        self.screen = pygame.display.set_mode(self.size)
+
+        # TODO Initialize all class veriables here
+        self.enemies = []
+        self.towers = []
+
+    def start(self):
+        while 1:
+            self.update()
+            self.draw()
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def draw(self):
+        self.screen.fill([0, 0, 0])
+        pygame.display.flip()
---
+++ main.py
@@ -0,0 +1,4 @@
+from game import Game
+
+g = Game(1200, 800)
+g.start()
